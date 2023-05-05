5631133844273bef1d173d4915bb825d138cef8f
Amanda Gomes <127699422+AmandaG1s@users.noreply.github.com>
2023-05-05 10:54:09 -0300

    Add files via upload

---
+++ Lab_extra.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+//Amanda Gomes
+int main()
+{
+   char str[100];
+    int i,j;
+    printf("Escrava:");
+    fgets(str,100,stdin);
+    for(i=0;i<100;i++)
+    {
+    if(str[i]=='r' && str[i+1]==' ')
+    continue;
+    }
+    else if(str[i] == 'r' && str[i+1] == 'r'){
+    str[i] ='l';
+    for(j=i+1;j <100;j++){
+    str[j] = str[j+1];
+   
+    }
+    }
+    else if(str[i]=='r'){
+    str[i]='l';
+    }
+    printf("\nFicara %s",str);
+}
